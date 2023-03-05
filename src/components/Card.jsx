047fe02c91f419ca28c21283c9740bee72b9195a
Aricoins <arielgarcia79@gmail.com>
2023-03-05 20:23:09 -0300

    230305

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,42 +1,63 @@
 import styled from "styled-components";
 
-
+const ImagenBrilla =styled.h2 `
+opacity: 1;
+   :hover{
+      filter: brightness(1.2)
+   }
+`
 const EstiloDiv=styled.div`
-background-color: #020005 ;
+background-color: #131016; 
 display: inline-flex;
+opacity: 0.9;
 height: 45%;
-color: white;
+border-radius: 10px;
 
 `
 
 
 const Style = styled.h2`
-background-color: #636563;
+
 color: white;
 :hover{
-background-color: #f256b3;
+background-color: #7b0808 ;
+-webkit-text-stroke: 1px white;
 color: black;
-
 }
 `
 
-
-const ImagenBrilla =styled.h2 `
-   :hover{
-      filter: brightness(1.2)
-   }
+const BotonBorde = styled.button`
+background-color: #7b0808 ;
+align-content: flex-end;
+height: 5%;
+width: 8%;
+padding: 2%;
+color: #e1d9d9;
+margin-left: 80%;
+border-color: #e1d9d9;
 `
+const Propss = styled.h3`
+text-shadow: 1px  0px 0px ,
+               1px  1px 0px white,
+              -1px  0px 0px white,
+               1px -1px 0px white;
+               :hover{
+                  background-color: azure;
+               }
+`
+
+
 
 export default function Card(props) {
 
 
    return (
      <EstiloDiv> <div>
-         <button onClick={props.onClose}>X</button>
+        <BotonBorde onClick={props.onClose}> X </BotonBorde>
          <Style><h2>{props.name}</h2></Style>
-        <h2>{props.species}</h2>
-         <h2>{props.gender}</h2>
-         <h3>ID: {props.id}</h3>
+         <Propss> <h3>{props.species}</h3></Propss>
+         <Propss><h3>{props.gender}</h3></Propss>
+         <Propss><h3>ID: {props.id}</h3></Propss>
          <ImagenBrilla><img src={props.image} alt="Acá debería verse una imagen" /></ImagenBrilla>
       </div></EstiloDiv>
    );
