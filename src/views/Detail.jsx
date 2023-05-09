15362692d9f4084889d78de2a1bc75d642364525
Aricoins <arielgarcia79@gmail.com>
2023-05-09 06:55:54 -0300

    230509 7h

--- src/views/Detail.jsx
+++ src/views/Detail.jsx
@@ -30,7 +30,6 @@ function Detail(props) {
   const { character , setCharacter} = props;
   
   useEffect(() => {
-<<<<<<< HEAD
    axios(`https://rickandmortyapi.com/api/character/${id}`).then(({ data }) => {
       if (data.name) {
          setCharacter(data);
@@ -40,22 +39,7 @@ function Detail(props) {
    });
    return setCharacter({});
 }, [id, setCharacter]);
-=======
-          axios(`https://rickandmortyapi.com/api/character/${id}`)
-      .then(({ data }) => {
-        if (data.name) {
-          setCharacter(data);
-        } else {
-          window.alert('No hay personajes con ese ID');
-        }
-      })
-      .catch((error) => {
-        console.error(error);
-        window.alert('Ocurrió un error al cargar el personaje');
-      });
-  }, [id]);
->>>>>>> f8aa6c6f5d931f3aa85a57c2b63c06990c351645
-  
+        
   return (<>
    <h1>{id}</h1>
       <img alt="Hola" src={character.image} /> 
